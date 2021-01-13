2859f7f75145607fd4f9410f336d788f9a5a2c30
kouhei yamanaka <home3751@gmail.com>
2021-01-14 02:01:14 +0900

    add,checked,delete処理機能ＯＫ

--- src/App.js
+++ src/App.js
@@ -2,26 +2,56 @@ import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
-
-//コールバック変数処理
 
 function App(props) {
+//タスクのデータを保持（フック）
+  const [tasks, setTasks] = useState(props.tasks);
+ 
+  //以下、コールバックpropsの処理
+  //name(form入力のvalue)を受け取り　⓵オブジェクトに変換　⓶フックを呼び出して追加
   function addTask(name) {
-    const newTask = { id: "id", name: name, completed: false };
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
-  const [tasks, setTasks] = useState(props.tasks);
+  //チェックボックス機能（htmlのcheckbox要素と同期させる処理)
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);　//⇒idが一致したらリストにアップする
+  }
+
+  //（リストの）削除ボタン機能⇒idが一致しないものを残す処理
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  //
   const taskList = tasks.map(task => (
     <Todo
         id={task.id}
         name={task.name}
         completed={task.completed}
         key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
       />
     )
   );
-  return (
+
+  //リストのコメント部分の処理
+  const taskNum = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${taskNum} remaining`;
+  
+    return (
     <div className="todoapp stack-large">
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
@@ -29,7 +59,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
