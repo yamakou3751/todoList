01c5718c94649822513f5547504af94ea09a82ee
kouhei yamanaka <home3751@gmail.com>
2021-01-15 01:08:46 +0900

    editボタンで編集画面を出す処理まで（機能は未設置）

--- src/App.js
+++ src/App.js
@@ -29,10 +29,23 @@ function App(props) {
     setTasks(updatedTasks);　//⇒idが一致したらリストにアップする
   }
 
-  //（リストの）削除ボタン機能⇒idが一致しないものを残す処理
+  //（リストの）削除ボタン機能⇒idが一致しないものをstateで保持(=一致したら置換されるので削除される)
   function deleteTask(id) {
     const remainingTasks = tasks.filter(task => id !== task.id);
     setTasks(remainingTasks);
+  }
+
+  //エディット機能
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //オブジェクトの一部（name）を変更する
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
   }
   //
   const taskList = tasks.map(task => (
@@ -43,6 +56,7 @@ function App(props) {
         key={task.id}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
+        edittTask={editTask}
       />
     )
   );
@@ -50,7 +64,7 @@ function App(props) {
   //リストのコメント部分の処理
   const taskNum = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${taskNum} remaining`;
-  
+
     return (
     <div className="todoapp stack-large">
       <Form addTask={addTask} />
