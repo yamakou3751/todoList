8485f7b3157fe7c5c5bc7bdadb1772cffc5b4b19
kouhei yamanaka <home3751@gmail.com>
2021-01-16 19:58:45 +0900

    'filter機能=

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
 
+//フィルターstateの其々の処理⇒key配列(タスクname)をFILTER_NAMESに格納
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+//key(ALL,Active,Completed)をFILTER_NAMESに格納
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
 //タスクのデータを保持（フック）
   const [tasks, setTasks] = useState(props.tasks);
+
+  //フィルターstate管理
+  const [filter, setFilter] = useState('All');
  
   //以下、コールバックpropsの処理
   //name(form入力のvalue)を受け取り　⓵オブジェクトに変換　⓶フックを呼び出して追加
@@ -47,19 +59,30 @@ function App(props) {
     });
     setTasks(editedTaskList);
   }
-  //
-  const taskList = tasks.map(task => (
+
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
     <Todo
-        id={task.id}
-        name={task.name}
-        completed={task.completed}
-        key={task.id}
-        toggleTaskCompleted={toggleTaskCompleted}
-        deleteTask={deleteTask}
-        editTask={editTask}
-      />
-    )
-  );
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  //FilterButtonコンポーネントにprops(FILTERのkey)を渡す
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
 
   //リストのコメント部分の処理
   const taskNum = taskList.length !== 1 ? 'tasks' : 'task';
@@ -69,9 +92,7 @@ function App(props) {
     <div className="todoapp stack-large">
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+      {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,10 +1,20 @@
 import React from "react";
 
+//button機能の開設
+// ①子要素　名前はFILTERのkeyを受け取る　※他は多分class属性の為
+// ②クリックイベントでAppコンポネのsetFilterフックをnameに更新
+// ⓷（②より）stateがクリックされたFilterコンポネのnameなので、クリックされたbuttonのaria-pressedだけtrue（他はfalse）になる
+//
 function FilterButton(props) {
   return (
-    <button type="button" className="btn toggle-btn" aria-pressed="true">
+    <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.name)}
+    >
       <span className="visually-hidden">Show </span>
-      <span>all </span>
+      <span>{props.name}</span>
       <span className="visually-hidden"> tasks</span>
     </button>
   );
