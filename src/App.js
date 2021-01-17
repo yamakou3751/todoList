8778efbdb664d337f32964571c0efc6f73789eae
yamakou3751 <67227033+yamakou3751@users.noreply.github.com>
2021-01-17 10:33:04 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -97,7 +97,6 @@ function App(props) {
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
-        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
