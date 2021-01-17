76ac375d8c2e2c2bea2267cec5d41d32098ad685
kouhei yamanaka <home3751@gmail.com>
2021-01-17 10:05:35 +0900

    仮のbranch

--- src/App.js
+++ src/App.js
@@ -96,11 +96,7 @@ function App(props) {
       {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
+      <ul>
         {taskList}
       </ul>
     </div>
